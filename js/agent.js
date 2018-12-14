2be9e847f1cf6d92ac42341aa027fd0724c5f979
lab-pc1 <cbc106008@nptu.edu.tw>
2018-12-14 18:20:46 +0800

    煥然一新的介面。

--- js/agent.js
+++ js/agent.js
@@ -40,6 +40,59 @@ TxtType.prototype.tick = function () {
     }, delta);
 };
 
+function SmoothScroll(target, speed, smooth) {
+    if (target == document)
+        target = (document.documentElement || document.body.parentNode || document.body) // cross browser support for document scrolling
+    var moving = false
+    var pos = target.scrollTop
+    target.addEventListener('mousewheel', scrolled, false)
+    target.addEventListener('DOMMouseScroll', scrolled, false)
+
+    function scrolled(e) {
+        e.preventDefault(); // disable default scrolling
+
+        var delta = normalizeWheelDelta(e)
+
+        pos += -delta * speed
+        pos = Math.max(0, Math.min(pos, target.scrollHeight - target.clientHeight)) // limit scrolling
+
+        if (!moving) update()
+    }
+
+    function normalizeWheelDelta(e) {
+        if (e.detail) {
+            if (e.wheelDelta)
+                return e.wheelDelta / e.detail / 40 * (e.detail > 0 ? 1 : -1) // Opera
+            else
+                return -e.detail / 3 // Firefox
+        } else
+            return e.wheelDelta / 120 // IE,Safari,Chrome
+    }
+
+    function update() {
+        moving = true
+        var delta = (pos - target.scrollTop) / smooth
+        target.scrollTop += delta
+        if (Math.abs(delta) > 0.5)
+            requestFrame(update)
+        else
+            moving = false
+    }
+
+    var requestFrame = function () { // requestAnimationFrame cross browser
+        return (
+            window.requestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            window.oRequestAnimationFrame ||
+            window.msRequestAnimationFrame ||
+            function (func) {
+                window.setTimeout(func, 1000 / 50);
+            }
+        );
+    }()
+}
+
 window.onload = function () {
     var elements = document.getElementsByClassName('typewrite');
     for (var i = 0; i < elements.length; i++) {
@@ -54,4 +107,44 @@ window.onload = function () {
     css.type = "text/css";
     css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
     document.body.appendChild(css);
+
+    $.scrollify({
+        section: ".row",
+        scrollSpeed: 1500,
+        before:function(i,panels) {
+
+            var ref = panels[i].attr("data-section-name");
+      
+            $(".swap .active").removeClass("active");
+      
+            $(".swap").find("a[href=\"#" + ref + "\"]").addClass("active");
+          },
+          afterRender:function() {
+            var swap = "<ul class=\"swap\">";
+            var activeClass = "";
+            $(".row").each(function(i) {
+              activeClass = "";
+              if(i===0) {
+                activeClass = "active";
+              }
+              swap += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span class=\"hover-text\">" + $(this).attr("data-section-name").charAt(0).toUpperCase() + $(this).attr("data-section-name").slice(1) + "</span></a></li>";
+            });
+      
+            swap += "</ul>";
+      
+            $("body").append(swap);
+            /*
+      
+            Tip: The two click events below are the same:
+      
+            $(".swap a").on("click",function() {
+              $.scrollify.move($(this).attr("href"));
+            });
+      
+            */
+            $(".swap a").on("click",$.scrollify.move);
+          }
+    });
+    // SmoothScroll(document, 120, 20);
+
 };
