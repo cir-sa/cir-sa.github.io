55a765c7735a98e1772f522f91ac0c5487af5acc
Chun Hu <cbc106008@stmail.nptu.edu.tw>
2018-12-22 16:04:17 +0800

    加入動畫效果

--- js/agent.js
+++ js/agent.js
@@ -24,7 +24,9 @@ TxtType.prototype.tick = function () {
     var that = this;
     var delta = 200 - Math.random() * 100;
 
-    if (this.isDeleting) { delta /= 2; }
+    if (this.isDeleting) {
+        delta /= 2;
+    }
 
     if (!this.isDeleting && this.txt === fullTxt) {
         delta = this.period;
@@ -108,30 +110,106 @@ window.onload = function () {
     css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
     document.body.appendChild(css);
 
+    var buf_section = -1;
+
+    $.fn.extend({
+        animateCss: function (animationName, callback) {
+            var animationEnd = (function (el) {
+                var animations = {
+                    animation: 'animationend',
+                    OAnimation: 'oAnimationEnd',
+                    MozAnimation: 'mozAnimationEnd',
+                    WebkitAnimation: 'webkitAnimationEnd',
+                };
+
+                for (var t in animations) {
+                    if (el.style[t] !== undefined) {
+                        return animations[t];
+                    }
+                }
+            })(document.createElement('div'));
+
+            this.addClass('animated ' + animationName).one(animationEnd, function () {
+                $(this).removeClass('animated ' + animationName);
+
+                if (typeof callback === 'function') callback();
+            });
+
+            return this;
+        },
+    });
+
     $.scrollify({
         section: ".row",
         scrollSpeed: 1500,
-        before:function(i,panels) {
-
+        // touchScroll: false,
+        // scrollbars: false,
+        before: function (i, panels) {
             var ref = panels[i].attr("data-section-name");
-      
             $(".swap .active").removeClass("active");
-      
             $(".swap").find("a[href=\"#" + ref + "\"]").addClass("active");
-          },
-          afterRender:function() {
+            $('meta[name=theme-color]').attr("content", panels[i].css("background-color"));
+            
+            var prev_i;
+            if (i - buf_section > 0) {
+                if (i > 0) {
+                    prev_i = i-1;
+                }
+            } else {
+                if (i < 5) {
+                    prev_i = i+1;
+                }
+            }
+            buf_section = i;
+
+            var effectIn = 'bounceIn';
+            var effectOut = 'fadeOut';
+            if (i == 0) {
+                effectIn += 'Up';
+            } else if (i == 1) {
+                effectIn += 'Left';
+            } else if (i == 2) {
+                effectIn += 'Down';
+            } else if (i == 3) {
+                effectIn += 'Right';
+            } else if (i == 4) {
+                effectIn += 'Up';
+            } else {
+                effectIn += 'Left';
+            }
+
+            if (prev_i == 0) {
+                effectOut += 'Down';
+            } else if (prev_i == 1) {
+                effectOut += 'Left';
+            } else if (prev_i == 2) {
+                effectOut += 'Up';
+            } else if (prev_i == 3) {
+                effectOut += 'Right';
+            } else if (prev_i == 4) {
+                effectOut += 'Down';
+            } else {
+                effectOut += 'Left';
+            }
+            panels[prev_i].children().animateCss(effectOut);
+            panels[i].children().animateCss(effectIn);
+        },
+        after: function (i, panels) {
+            panels[i].find("button").animateCss("shake");
+        },
+        afterRender: function () {
             var swap = "<ul class=\"swap\">";
             var activeClass = "";
-            $(".row").each(function(i) {
-              activeClass = "";
-              if(i===0) {
-                activeClass = "active";
-              }
-              swap += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span class=\"hover-text\">" + $(this).attr("data-section-name").charAt(0).toUpperCase() + $(this).attr("data-section-name").slice(1) + "</span></a></li>";
+            $(".row").each(function (i) {
+                activeClass = "";
+                if (i === 0) {
+                    activeClass = "active";
+                }
+                swap += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span class=\"hover-text\">" + $(this).attr("data-section-name").charAt(0).toUpperCase() + $(this).attr("data-section-name").slice(1) + "</span></a></li>";
             });
-      
+
             swap += "</ul>";
-      
+
             $("body").append(swap);
             /*
       
@@ -142,8 +220,9 @@ window.onload = function () {
             });
       
             */
-            $(".swap a").on("click",$.scrollify.move);
-          }
+            $(".swap a").on("click", $.scrollify.move);
+            $(".col").animateCss('fadeInRight');
+        }
     });
     // SmoothScroll(document, 120, 20);
 
