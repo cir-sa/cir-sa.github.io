3af08144c20b225598e46ed3ad5c66fb3005151e
Jun Hu <cbc106008@stmail.nptu.edu.tw>
2020-10-17 00:30:52 +0800

    Add cover video, refactor code.

--- js/agent.js
+++ js/agent.js
@@ -42,59 +42,6 @@ TxtType.prototype.tick = function () {
     }, delta);
 };
 
-function SmoothScroll(target, speed, smooth) {
-    if (target == document)
-        target = (document.documentElement || document.body.parentNode || document.body) // cross browser support for document scrolling
-    var moving = false
-    var pos = target.scrollTop
-    target.addEventListener('mousewheel', scrolled, false)
-    target.addEventListener('DOMMouseScroll', scrolled, false)
-
-    function scrolled(e) {
-        e.preventDefault(); // disable default scrolling
-
-        var delta = normalizeWheelDelta(e)
-
-        pos += -delta * speed
-        pos = Math.max(0, Math.min(pos, target.scrollHeight - target.clientHeight)) // limit scrolling
-
-        if (!moving) update()
-    }
-
-    function normalizeWheelDelta(e) {
-        if (e.detail) {
-            if (e.wheelDelta)
-                return e.wheelDelta / e.detail / 40 * (e.detail > 0 ? 1 : -1) // Opera
-            else
-                return -e.detail / 3 // Firefox
-        } else
-            return e.wheelDelta / 120 // IE,Safari,Chrome
-    }
-
-    function update() {
-        moving = true
-        var delta = (pos - target.scrollTop) / smooth
-        target.scrollTop += delta
-        if (Math.abs(delta) > 0.5)
-            requestFrame(update)
-        else
-            moving = false
-    }
-
-    var requestFrame = function () { // requestAnimationFrame cross browser
-        return (
-            window.requestAnimationFrame ||
-            window.webkitRequestAnimationFrame ||
-            window.mozRequestAnimationFrame ||
-            window.oRequestAnimationFrame ||
-            window.msRequestAnimationFrame ||
-            function (func) {
-                window.setTimeout(func, 1000 / 50);
-            }
-        );
-    }()
-}
-
 window.onload = function () {
     var elements = document.getElementsByClassName('typewrite');
     for (var i = 0; i < elements.length; i++) {
@@ -110,7 +57,7 @@ window.onload = function () {
     css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
     document.body.appendChild(css);
 
-    var buf_section = -1;
+    var prev_i;
 
     $.fn.extend({
         animateCss: function (animationName, callback) {
@@ -141,7 +88,7 @@ window.onload = function () {
 
     $.scrollify({
         section: ".row",
-        scrollSpeed: 1500,
+        scrollSpeed: 1000,
         // touchScroll: false,
         // scrollbars: false,
         before: function (i, panels) {
@@ -149,53 +96,26 @@ window.onload = function () {
             $(".swap .active").removeClass("active");
             $(".swap").find("a[href=\"#" + ref + "\"]").addClass("active");
             $('meta[name=theme-color]').attr("content", panels[i].css("background-color"));
-            
-            var prev_i;
-            if (i - buf_section > 0) {
-                if (i > 0) {
-                    prev_i = i-1;
-                }
-            } else {
-                if (i < 5) {
-                    prev_i = i+1;
-                }
-            }
-            buf_section = i;
 
-            var effectIn = 'bounceIn';
-            var effectOut = 'fadeOut';
-            if (i == 0) {
-                effectIn += 'Up';
-            } else if (i == 1) {
-                effectIn += 'Left';
-            } else if (i == 2) {
-                effectIn += 'Down';
-            } else if (i == 3) {
-                effectIn += 'Right';
-            } else if (i == 4) {
-                effectIn += 'Up';
-            } else {
-                effectIn += 'Left';
+            if (prev_i) {
+                var effectOut_list = ['Down', 'Left', 'Up', 'Right', 'Down', 'Left'];
+                var effectOut = 'fadeOut' + effectOut_list[prev_i];
+                panels[prev_i].children().animateCss(effectOut);
             }
 
-            if (prev_i == 0) {
-                effectOut += 'Down';
-            } else if (prev_i == 1) {
-                effectOut += 'Left';
-            } else if (prev_i == 2) {
-                effectOut += 'Up';
-            } else if (prev_i == 3) {
-                effectOut += 'Right';
-            } else if (prev_i == 4) {
-                effectOut += 'Down';
+            var effectIn_list = ['Up', 'Up', 'Up', 'Up', 'Up', 'Up',];
+            if (prev_i && i - prev_i < 0) {
+                var effectIn = 'delay-custom fadeInUp';
             } else {
-                effectOut += 'Left';
+                var effectIn = 'delay-custom fadeInDown';
             }
-            panels[prev_i].children().animateCss(effectOut);
-            panels[i].children().animateCss(effectIn);
+            console.log(effectIn);
+            panels[i].children('.col').animateCss(effectIn);
+
+            prev_i = i
         },
         after: function (i, panels) {
-            panels[i].find("button").animateCss("swing");
+            panels[i].find(".after_swing").animateCss("swing");
         },
         afterRender: function () {
             var swap = "<ul class=\"swap\">";
@@ -225,5 +145,9 @@ window.onload = function () {
         }
     });
     // SmoothScroll(document, 120, 20);
-
 };
+
+$().ready(function () {
+    $('.section1').tubular({ videoId: 'qBAo3-Fc3j0', start: 15})
+});
+//  where idOfYourVideo is the YouTube ID. })
